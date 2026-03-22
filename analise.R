d6023e17e51bb3e751d7a24f52143defa056d5db
Agnes <afkunsch@gmail.com>
2026-03-22 11:48:39 -0300

    Esse é o primeiro commit relativo ao exercício 3.

---
+++ analise.R
@@ -0,0 +1,15 @@
+---
+title: "Lista de Exercícios 2 - Aula 3"
+author: "Agnes"
+date: "2026-03-22"
+output: html_document
+---
+
+## **Questão 1**
+*Crie uma conta no GitHub (se ainda não tiver). Configure o Git no seu computador com seu nome e e-mail usando o terminal.*
+
+Minha conta: https://github.com/agneskunsch
+
+## **Questão 3**
+*No projeto clonado, crie um arquivo analise.R com qualquer código R simples. Faça o ciclo completo: stage, commit (com uma mensagem descritiva) e push. Verifique no GitHub se o arquivo apareceu.*
+
