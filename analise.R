6b86d8d6f298de936b2ed9110f9b69600d5b5b42
Agnes <afkunsch@gmail.com>
2026-03-22 12:15:07 -0300

    Para o exercício 5, estou salvando algumas alterações na branch "experimento"

--- analise.R
+++ analise.R
@@ -1,6 +1,6 @@
 #código simples qualquer
 
-códigoqualquer <- sum (2,2)
+códigoqualquer <- mean(c(200, 100))
 códigoqualquer
 
 install.packages ("usethis")
@@ -12,5 +12,5 @@ usethis::git_sitrep()
 
 #agora, para o exercício 4, estou adicionando mais linhas de código
 
-códigoqualquer2 <- sum (códigoqualquer, 7)
+códigoqualquer2 <- sum (códigoqualquer, 150)
 códigoqualquer2
